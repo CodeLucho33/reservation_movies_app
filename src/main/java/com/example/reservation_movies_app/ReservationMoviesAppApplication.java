852387dae4fba1dd73dbd3430296bc96ae509918
CodeLucho33 <code.lucho33@gmail.com>
2025-03-31 10:36:00 -0500

    first commit

---
+++ src/main/java/com/example/reservation_movies_app/ReservationMoviesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservation_movies_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationMoviesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationMoviesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservation_movies_app/ReservationMoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservation_movies_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationMoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
